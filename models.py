5caff9ee2502717d7033eeaa049732ea2336daaf
Mike Pirnat <mpirnat@gmail.com>
2017-10-26 23:14:42 -0400

    Added a Character model using attrs

---
+++ models.py
@@ -0,0 +1,28 @@
+from attr import attrs, attrib
+
+
+@attrs
+class Character:
+    name = attrib(default="")
+    race = attrib(default="Human")
+    cclass = attrib(default="Fighter")
+    level = attrib(default=1)
+    max_hp = attrib(default=10)
+    _cur_hp = attrib(default=10)
+    ac = attrib(default=10)
+    initiative_modifier = attrib(default=0)
+    perception = attrib(default=10)
+    darkvision = attrib(default=0)
+    status = attrib(default="Normal")
+
+    @property
+    def cur_hp(self):
+        return self._cur_hp
+
+    @cur_hp.setter
+    def cur_hp(self, value):
+        if value > self.max_hp:
+            value = self.max_hp
+        if value < 0:
+            value = 0
+        self._cur_hp = value
