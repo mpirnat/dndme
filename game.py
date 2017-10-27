c1780447269c973ce9171c620669a05a8886b2ad
Mike Pirnat <mpirnat@gmail.com>
2017-10-26 23:25:09 -0400

    Now use Character model in game test harness

--- game.py
+++ game.py
@@ -1,14 +1,19 @@
 import pytoml as toml
 from initiative import TurnManager
 from dice import roll_dice
+from models import Character
+
 
 party = {}
 with open('party.toml', 'rb') as fin:
     party = toml.load(fin)
 
+characters = [Character(**x) for x in party.values()]
+
 tm = TurnManager()
-for character in party.values():
-    tm.add_combatant(character, roll_dice(1, 20))
+for character in characters:
+    tm.add_combatant(character,
+            roll_dice(1, 20, modifier=character.initiative_modifier))
 turns = tm.generate_turns()
 for i in range(10):
     print(next(turns))
