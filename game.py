b7b6935e03dadb478ad37545b9b75808d3ddd14f
Mike Pirnat <mpirnat@gmail.com>
2017-10-29 10:31:14 -0400

    Load encounters and their monsters, with rolled or manually set hp

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
+import glob
 import pytoml as toml
 from initiative import TurnManager
 from dice import roll_dice
-from models import Character
+from models import Character, Encounter, Monster
 
+tm = TurnManager()
 
 party = {}
 with open('party.toml', 'rb') as fin:
@@ -10,7 +12,31 @@ with open('party.toml', 'rb') as fin:
 
 characters = [Character(**x) for x in party.values()]
 
-tm = TurnManager()
+with open('encounters/lmop1.1.1.toml', 'r') as fin:
+    encounter = Encounter(**toml.load(fin))
+    print(encounter)
+
+for group in encounter.groups.values():
+    available_monster_files = glob.glob('monsters/*.toml')
+    monsters = []
+    for filename in available_monster_files:
+        monster = toml.load(open(filename, 'r'))
+        if monster['name'] == group['monster']:
+            for i in range(group['count']):
+                monsters.append(Monster(**monster))
+            break
+    for i in range(len(monsters)):
+        if 'max_hp' in group and len(group['max_hp']) == len(monsters):
+            monsters[i].max_hp = group['max_hp'][i]
+            monsters[i].cur_hp = group['max_hp'][i]
+        else:
+            monsters[i].max_hp = monsters[i]._max_hp
+            monsters[i].cur_hp = monsters[i].max_hp
+    for monster in monsters:
+        tm.add_combatant(monster, roll_dice(1, 20,
+            modifier=monster.initiative_bonus))
+
+
 for character in characters:
     tm.add_combatant(character,
             roll_dice(1, 20, modifier=character.initiative_modifier))
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from attr import attrs, attrib
+import dice
 
 
 @attrs
@@ -28,3 +29,79 @@ class Character:
                 self.status = "Dead"
             value = 0
         self._cur_hp = value
+
+
+@attrs
+class Encounter:
+
+    name = attrib(default="")
+    location = attrib(default="")
+    notes = attrib(default="")
+    groups = attrib(default=[])
+
+
+@attrs
+class Monster:
+    name = attrib(default="")
+    race = attrib(default="")
+    cr = attrib(default=0)
+    xp = attrib(default=0)
+    size = attrib(default="medium")
+    mtype = attrib(default="humanoid")
+    alignment = attrib(default="unaligned")
+    str = attrib(default=10)
+    str_bonus = attrib(default=0)
+    dex = attrib(default=10)
+    dex_bonus = attrib(default=0)
+    con = attrib(default=10)
+    con_bonus = attrib(default=0)
+    int = attrib(default=10)
+    int_bonus = attrib(default=0)
+    wis = attrib(default=10)
+    wis_bonus = attrib(default=0)
+    cha = attrib(default=10)
+    cha_bonus = attrib(default=0)
+    initiative_bonus = attrib(default=0)
+    ac = attrib(default=0)
+    armor = attrib(default=[])
+    _max_hp = attrib(default=10)
+    _cur_hp = attrib(default=None)
+    speed = attrib(default=30)
+    stealth = attrib(default=0)
+    darkvision = attrib(default=0)
+    perception = attrib(default=10)
+    languages = attrib(default=[])
+    notes = attrib(default="")
+    skills = attrib(default=[])
+    attacks = attrib(default=[])
+    status = attrib(default="Normal")
+
+    @property
+    def max_hp(self):
+        return self._max_hp
+
+    @max_hp.setter
+    def max_hp(self, value):
+        try:
+            self._max_hp = int(value)
+        except ValueError:
+            # we have an expression for max hp, so roll it
+            self._max_hp = dice.roll_dice_expr(value)
+        # setting max_hp for the first time? we should set cur_hp too
+        if self.cur_hp is None:
+            self.cur_hp = self._max_hp
+
+    @property
+    def cur_hp(self):
+        return self._cur_hp
+
+    @cur_hp.setter
+    def cur_hp(self, value):
+        if value > self.max_hp:
+            value = self.max_hp
+        if value < 0:
+            if abs(value) >= self.max_hp:
+                self.status = "Dead"
+            value = 0
+        self._cur_hp = value
+
