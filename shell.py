29f43ad43ac6802826fff140474deff9a3e06915
Mike Pirnat <mpirnat@gmail.com>
2017-10-29 12:23:20 -0400

    Added basic commands to shell: command list, help, quit

--- shell.py
+++ shell.py
@@ -1,7 +1,107 @@
+import sys
+
+
+commands = {}
+
+class Command:
+
+    keywords = ['command']
+
+    def __init__(self):
+        for kw in self.keywords:
+            commands[kw] = self
+        print("Registered "+self.__class__.__name__)
+
+    def do_command(self, *args):
+        print("Nothing happens.")
+
+    def show_help_text(self, keyword):
+        if hasattr(self, 'help_text'):
+            divider = "-" * len(keyword)
+            print(self.help_text.format(**locals()))
+        else:
+            print("No help text available for: "+keyword)
+
+
+class ListCommands(Command):
+
+    keywords = ['commands']
+    help_text = """{keyword}
+{divider}
+Summary: List available commands
+
+Usage: {keyword}
+"""
+
+    def do_command(self, *args):
+        print("Available commands:\n")
+        for keyword in list(sorted(commands.keys())):
+            print('*', keyword)
+        print()
+
+
+class Help(Command):
+
+    keywords = ['help']
+    help_text = """{keyword}
+{divider}
+Summary: Get help for a command.
+
+Usage: {keyword} <command>
+"""
+
+    def do_command(self, *args):
+        if not args:
+            self.show_help_text('help')
+            return
+
+        keyword = args[0]
+        command = commands.get(keyword)
+        if not command:
+            print("Unknown command: "+keyword)
+            return
+        command.show_help_text(keyword)
+
+    def show_help_text(self, keyword):
+        super().show_help_text(keyword)
+        ListCommands.do_command(self, *[])
+
+
+class Quit(Command):
+
+    keywords = ['quit', 'exit']
+    help_text = """{keyword}
+{divider}
+Summary: quit the shell
+
+Usage: {keyword}
+"""
+
+    def do_command(self, *args):
+        print("Goodbye!")
+        sys.exit(1)
+
+
+def register_commands():
+    ListCommands()
+    Help()
+    Quit()
+
+
 def main_loop():
     while True:
-        user_input = input("> ")
+        user_input = input("> ").split()
+        if not user_input:
+            continue
+
+        command = commands.get(user_input[0]) or None
+        if not command:
+            print("Unknown command.")
+            continue
+
+        command.do_command(*user_input[1:])
 
 
 if __name__ == '__main__':
+    register_commands()
     main_loop()
