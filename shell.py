f7d4ab01528014ed706284b8a79045bef3661f73
Mike Pirnat <mpirnat@gmail.com>
2017-11-26 00:10:36 -0500

    Modified Roll command to allow multiple dice expressions, eg to handle advantage/disadvantage

--- shell.py
+++ shell.py
@@ -392,23 +392,25 @@ class Roll(Command):
 {divider}
 Summary: Roll dice using a dice expression
 
-Usage: {keyword} <dice expression>
+Usage: {keyword} <dice expression> [<dice expression> ...]
 
 Examples:
 
     {keyword} 3d6
     {keyword} 1d20+2
     {keyword} 2d4-1
+    {keyword} 1d20 1d20
 """
 
     def do_command(self, *args):
-        dice_expr = args[0]
-        try:
-            result = roll_dice_expr(dice_expr)
-        except ValueError:
-            print(f"Invalid dice expression: {dice_expr}")
-            return
-        print(result)
+        results = []
+        for dice_expr in args:
+            try:
+                results.append(str(roll_dice_expr(dice_expr)))
+            except ValueError:
+                print(f"Invalid dice expression: {dice_expr}")
+                return
+        print(', '.join(results))
 
 
 class SetCondition(Command):
