2f3a8c09029cebfa30922dc3a4f4cf550c80f5be
Mike Pirnat <mpirnat@gmail.com>
2018-04-18 21:53:50 -0400

    Simple script to check toml data

---
+++ check_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+import pytoml as toml
+
+if __name__ == "__main__":
+    filenames = sys.argv[1:]
+    for filename in filenames:
+        try:
+            data = toml.load(open(filename, 'r'))
+        except:
+            print(f"Error in {filename}!")
+            raise
+    print("All good!")
