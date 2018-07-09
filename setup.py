98edd2d6ac02684fc10a89e692f864d5bdf54e65
Ben Freeman <bfreema8@kent.edu>
2018-07-09 19:16:24 -0400

    prompt-toolkit 2.0 is incompatible with 1.x - need to pin to 1.0.15

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'attrs',
         'click',
-        'prompt-toolkit',
+        'prompt-toolkit==1.0.15',
         'pytoml',
         'six',
         'wcwidth',
